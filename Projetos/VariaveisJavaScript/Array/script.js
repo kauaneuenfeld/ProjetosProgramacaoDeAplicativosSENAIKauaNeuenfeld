bb13320692ce36b92250ff5b618ee13515cc5fc8
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-06-02 08:14:37 -0300

    Atualizacao array

--- Projetos/VariaveisJavaScript/Array/script.js
+++ Projetos/VariaveisJavaScript/Array/script.js
@@ -1,3 +1,21 @@
 let usuarios = [20, "Carlos", "Maria"];
 
 console.log(usuarios);
+
+//acesando itens
+console.log(usuarios[1]);
+
+//adicionando item no final da array
+//push()
+usuarios.push("Pedro");
+console.log(usuarios);
+
+//remove o ultimo item
+//pop()
+usuarios.pop();
+console.log(usuarios);
+
+//remove o primeiro item
+//shift
+usuarios.shift;
+console.log(usuarios);
