41cfb6b1d00708c70559e88d35e7bf422a72d09f
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-06-24 09:57:15 -0300

    ok

--- Projetos/Estrutura_API/app.js
+++ Projetos/Estrutura_API/app.js
@@ -15,34 +15,60 @@ server.listen(3001, () => {
 }); 
 */
 
-const express = require('express');
+const express = require("express");
 const server = express();
 
-const cursos = ['Nodejs', 'JavaScript', 'React Native'];
+server.use(express.json());
+
+const cursos = ["Nodejs", "JavaScript", "React Native"];
 //Query params = ?nome=Nodejs
 //route params = /curso/2
 //request body = { "nome": "Nodejs", "tipo": "Backend" }
-server.get('/cursos', (req, res) => {
-    const nome = req.query.nome;
-    return res.json({curso: `Aprendendo ${nome}`});
+server.get("/cursos", (req, res) => {
+  const nome = req.query.nome;
+  return res.json({ curso: `Aprendendo ${nome}` });
 });
 
 //Rota 2 com: Route params
-server.get('/curso/:id', (req, res) => {
-    const id = req.params.id;
-    return res.json(cursos[id]);
-}); 
+server.get("/curso/:id", (req, res) => {
+  const id = req.params.id;
+  return res.json(cursos[id]);
+});
 
 //========================================
 //Metodo hhttp GET
 //Listar todos os cursos
 //Localhost:3055/cursos
-server.get('/curso', (req, res) => {
-    return res.json(cursos);
+server.get("/curso", (req, res) => {
+  return res.json(cursos);
 });
 
+//Metodo http POST
+//Criando um novo curso
+//local host:3000/curso
+//{"nome": "Curo de Python"}
 
+server.post("/curso", (req, res) => {
+  //Desestrutura a propriedade "name" enviada no corpo da requisição
+  const nome = req.body.nome;
+  //adicina nvo curso no array cursos
+  cursos.push(nome);
+  //retorna a lista de cursos atualizada
+  return res.json(cursos);
+});
 
+//metodo http PUT
+//atualizar um curso
+//localhost:3000/curso/0
+server.put('/curso/:id', (req, res) => {
+
+    const id = req.params.id
+    const nome = req.body.nome
+
+    cursos[id] = nome
+    return res.json(cursos)
+})
 server.listen(3055, () => {
-    console.log('Servidor rodando na porta 3055');
+  console.log("Servidor rodando na porta 3055");
 });
+
