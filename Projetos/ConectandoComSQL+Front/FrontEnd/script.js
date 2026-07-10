2030767f646dc65610a1639b2e58b25d2b84ef34
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-07-10 08:37:47 -0300

    ok

--- Projetos/ConectandoComSQL+Front/FrontEnd/script.js
+++ Projetos/ConectandoComSQL+Front/FrontEnd/script.js
@@ -10,7 +10,28 @@ async function listarCursos() {
         lista.innerHTML += `
             <li>
                 ${curso.id} - ${curso.nome}
+                <button>Editar</button>
+                <button>Excluir</button>
             </li>
         `;
     });
+}
+
+async function cadastrarCursos() {
+    const nome = document.getElementById('nome').value;
+    if (nome === '') {
+        alert('Preencha o nome do curso');
+        return;
+    }
+    const resposta = await fetch('http://localhost:3023/cursos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nome })
+    });
+    const dados = await resposta.json();
+    alert(dados.mensagem);
+    document.getElementById('nome').value = '';
+    listarCursos();
 }
